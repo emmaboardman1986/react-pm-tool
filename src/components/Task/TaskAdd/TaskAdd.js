f539ba15fccb669cb4d9f2708c828a76376069cc
E2 <e2@macbook-pro-5.home>
2019-07-16 08:46:42 +0100

    test new branch push

--- src/components/Task/TaskAdd/TaskAdd.js
+++ src/components/Task/TaskAdd/TaskAdd.js
@@ -51,7 +51,6 @@ const AddTask = props => {
     >
       {project.clientName + ": " + project.projectName}
     </option>
-
   ));
 
   const impactOptions = ["Low", "Medium", "High"];
@@ -88,8 +87,8 @@ const AddTask = props => {
             placeholder={availableTimes.endTime}
           />
         </div>
-	  );
-	}
+      );
+    }
   };
 
   const handleSubmit = event => {
@@ -101,11 +100,9 @@ const AddTask = props => {
         console.log(response);
         props.closeModal();
       })
-	  .catch(error => console.log(error));
-	  var t1 = performance.now();
-      console.log(
-        "Call to handleSubmit took " + (t1 - t0) + " milliseconds."
-      );
+      .catch(error => console.log(error));
+    var t1 = performance.now();
+    console.log("Call to handleSubmit took " + (t1 - t0) + " milliseconds.");
   };
 
   return (
@@ -113,137 +110,150 @@ const AddTask = props => {
       <div className={classes.AddTask} data-cy="add-task-modal">
         <h2>Add New Task</h2>
         <form onSubmit={handleSubmit}>
-		<div class={classes.TopOnDesktop}>
-        <div class={classes.LeftOnDesktop}>
-          <ul className={classes.FormWrapper}>
-            <li className={classes.FormRow}>
-              <label htmlFor="projectId">Project</label>
-              <select
-                name="projectId"
-                id="projectId"
-                onChange={handleInputChange}
-                value={formInput.projectId}
-                required
-              >
-                {generatedProjectOptions}
-              </select>
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskTitle">Task Title</label>
-              <input
-                type="text"
-                id="taskTitle"
-                name="taskTitle"
-                onBlur={handleInputChange}
-                required
-              />
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskArea">Affected area</label>
-              <input
-                type="text"
-                id="taskArea"
-                name="taskAffectedArea"
-                onBlur={handleInputChange}
-                required
-              />
-            </li>
-            <li className={classes.FormRowTextArea}>
-              <label htmlFor="taskError">Erroneous behaviour</label>
-              <textarea
-                name="taskErroneousBehaviour"
-                id="taskErroneousBehaviour"
-                onBlur={handleInputChange}
-                required
-              />
-            </li>
-            <li className={classes.FormRowTextArea}>
-              <label htmlFor="taskNormal">Expected behaviour</label>
-              <textarea
-				name="taskExpectedBehaviour"
-				id="taskExpectedBehaviour"
-                onBlur={handleInputChange}
-                required
-              />
-            </li>
-			</ul>
-			  </div>
-       		 <div class={classes.RightOnDesktop}>
-				<ul className={classes.FormWrapper}>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskImpact">Impact on business</label>
-              <select
-				name="taskImpact"
-				id="taskImpact"
-                onChange={handleInputChange}
-                value={formInput.taskImpact}
-                required
-              >
-                {generatedImpactOptions}
-              </select>
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskProblemSince">Time noticed</label>
-              <input
-                type="text"
-                id="taskTimeNoticed"
-                name="taskTimeNoticed"
-                onBlur={handleInputChange}
-                required
-              />
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskRecentChanges">Recent Changes</label>
-              <input
-                type="text"
-                id="taskRecentChanges"
-                name="taskRecentChanges"
-              />
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="resourceId">Resource</label>
-              <select
-                name="taskResource"
-				onChange={handleInputChange}
-				id="resourceId"
-                value={formInput.resourceId}
-                required
-              >
-                {generatedResourceOptions}
-              </select>
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskEstimate">Estimate</label>
-              <input
-                type="text"
-                id="taskEstimate"
-                name="taskEstimate"
-                onChange={handleInputChange}
-                onBlur={() => {
-                  props.handleSchedulePlacement({
-                    resourceId: formInput.taskResource,
-                    taskEstimate: formInput.taskEstimate
-                  });
-                }}
-                required
-              />
-            </li>
-            <li className={classes.FormRow}>
-              <label htmlFor="taskSchedule">First Available Time Slot</label>
-            <p class={classes.DefaultMessage}>{(props.availableTimes.endTime) ? generateTimeSlots(props.availableTimes) : "Requires resource and estimate input"}</p>
-            </li>
-			
-          </ul>
-		  </div>
-		  </div>
-		  <div class="bottom-on-desktop">
-          <button type="submit" data-cy="add-task-submit" className={classes.AddTaskBtn}>
-            Add Task
-          </button>
-          <p className={classes.CancelBtn} onClick={props.closeModal} data-cy="add-task-cancel">
-            Cancel
-          </p>
-		  </div>
+          <div class={classes.TopOnDesktop}>
+            <div class={classes.LeftOnDesktop}>
+              <ul className={classes.FormWrapper}>
+                <li className={classes.FormRow}>
+                  <label htmlFor="projectId">Project</label>
+                  <select
+                    name="projectId"
+                    id="projectId"
+                    onChange={handleInputChange}
+                    value={formInput.projectId}
+                    required
+                  >
+                    {generatedProjectOptions}
+                  </select>
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskTitle">Task Title</label>
+                  <input
+                    type="text"
+                    id="taskTitle"
+                    name="taskTitle"
+                    onBlur={handleInputChange}
+                    required
+                  />
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskArea">Affected area</label>
+                  <input
+                    type="text"
+                    id="taskArea"
+                    name="taskAffectedArea"
+                    onBlur={handleInputChange}
+                    required
+                  />
+                </li>
+                <li className={classes.FormRowTextArea}>
+                  <label htmlFor="taskError">Erroneous behaviour</label>
+                  <textarea
+                    name="taskErroneousBehaviour"
+                    id="taskErroneousBehaviour"
+                    onBlur={handleInputChange}
+                    required
+                  />
+                </li>
+                <li className={classes.FormRowTextArea}>
+                  <label htmlFor="taskNormal">Expected behaviour</label>
+                  <textarea
+                    name="taskExpectedBehaviour"
+                    id="taskExpectedBehaviour"
+                    onBlur={handleInputChange}
+                    required
+                  />
+                </li>
+              </ul>
+            </div>
+            <div class={classes.RightOnDesktop}>
+              <ul className={classes.FormWrapper}>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskImpact">Impact on business</label>
+                  <select
+                    name="taskImpact"
+                    id="taskImpact"
+                    onChange={handleInputChange}
+                    value={formInput.taskImpact}
+                    required
+                  >
+                    {generatedImpactOptions}
+                  </select>
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskProblemSince">Time noticed</label>
+                  <input
+                    type="text"
+                    id="taskTimeNoticed"
+                    name="taskTimeNoticed"
+                    onBlur={handleInputChange}
+                    required
+                  />
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskRecentChanges">Recent Changes</label>
+                  <input
+                    type="text"
+                    id="taskRecentChanges"
+                    name="taskRecentChanges"
+                  />
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="resourceId">Resource</label>
+                  <select
+                    name="taskResource"
+                    onChange={handleInputChange}
+                    id="resourceId"
+                    value={formInput.resourceId}
+                    required
+                  >
+                    {generatedResourceOptions}
+                  </select>
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskEstimate">Estimate</label>
+                  <input
+                    type="text"
+                    id="taskEstimate"
+                    name="taskEstimate"
+                    onChange={handleInputChange}
+                    onBlur={() => {
+                      props.handleSchedulePlacement({
+                        resourceId: formInput.taskResource,
+                        taskEstimate: formInput.taskEstimate
+                      });
+                    }}
+                    required
+                  />
+                </li>
+                <li className={classes.FormRow}>
+                  <label htmlFor="taskSchedule">
+                    First Available Time Slot
+                  </label>
+                  <p class={classes.DefaultMessage}>
+                    {props.availableTimes.endTime
+                      ? generateTimeSlots(props.availableTimes)
+                      : "Requires resource and estimate input"}
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="bottom-on-desktop">
+            <button
+              type="submit"
+              data-cy="add-task-submit"
+              className={classes.AddTaskBtn}
+            >
+              Add Task
+            </button>
+            <p
+              className={classes.CancelBtn}
+              onClick={props.closeModal}
+              data-cy="add-task-cancel"
+            >
+              Cancel
+            </p>
+          </div>
         </form>
       </div>
     </React.Fragment>
